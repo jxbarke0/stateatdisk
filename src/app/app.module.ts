52c56b3b92462d1690f71a5ebafcf94194d69627
Jeff B <jeff.r.barker@gmail.com>
2019-08-29 11:58:28 -0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { RegistrationFormComponent } from "./registration-form/registration-form.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, RegistrationFormComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/registration-form/registration-form.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from "@angular/core";
+import { RegistrationService } from "../registration.service";
+import { FormBuilder, Validators, AbstractControl } from "@angular/forms";
+
+@Component({
+  selector: "app-registration-form",
+  templateUrl: "./registration-form.html",
+  styles: []
+})
+export class RegistrationFormComponent implements OnInit {
+  submitted = false;
+  errorMsg = "";
+
+  constructor(
+    private _registrationService: RegistrationService,
+    private fb: FormBuilder
+  ) {}
+  // Registration Form
+  registrationForm = this.fb.group(
+    {
+      userName: ["", Validators.required],
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", Validators.required],
+      confirmPassword: ["", Validators.required]
+    },
+    { validator: this.PasswordValidator }
+  );
+
+  // Handles form validation errors
+  validationErrors = false;
+
+  PasswordValidator(
+    control: AbstractControl
+  ): { [key: string]: boolean } | null {
+    const password = control.get("password");
+    const confirmPassword = control.get("confirmPassword");
+    if (password.pristine || confirmPassword.pristine) {
+      return null;
+    }
+    return password &&
+      confirmPassword &&
+      password.value !== confirmPassword.value
+      ? { misMatch: true }
+      : null;
+  }
+
+  onSubmit() {
+    console.log(this.registrationForm);
+    this.submitted = true;
+    this._registrationService
+      .register(this.registrationForm.value)
+      .subscribe(
+        response => console.log("Success", response),
+        error => (this.errorMsg = error.statusText)
+      );
+  }
+
+  resetForm() {
+    this.submitted = false;
+    this.errorMsg = "";
+  }
+
+  ngOnInit() {
+    console.log(this.registrationForm);
+  }
+}
